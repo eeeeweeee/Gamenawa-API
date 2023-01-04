baa991180f10332219410551a89a2c50e104a1a7
Rabongbong <didgmltn7769@naver.com>
2023-01-04 16:29:10 +0900

    init

---
+++ src/main/java/com/ewe/gamenawa/GamenawaApplication.java
@@ -0,0 +1,13 @@
+package com.ewe.gamenawa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamenawaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamenawaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ewe/gamenawa/GamenawaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewe.gamenawa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamenawaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
