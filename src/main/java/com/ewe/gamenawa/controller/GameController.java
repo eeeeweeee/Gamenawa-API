afc91c798d23fa5689b23998f3ed8946b8ffd122
Rabongg <didgmltn7769@naver.com>
2023-02-27 17:52:31 +0900

    feat: 임시로 cors 허용 (#2)

--- src/main/java/com/ewe/gamenawa/controller/GameController.java
+++ src/main/java/com/ewe/gamenawa/controller/GameController.java
@@ -4,12 +4,10 @@ import com.ewe.gamenawa.dto.GameListResponse;
 import com.ewe.gamenawa.service.GameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 @RequestMapping("/games")
 @RequiredArgsConstructor
 public class GameController {
